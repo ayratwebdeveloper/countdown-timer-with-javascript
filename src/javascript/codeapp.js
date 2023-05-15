5cc2e9b6b9f7aad2fcede79b113ab03ff0c31ed9
ayratdeveloper <ayratdeveloper@gmail.com>
2023-05-15 22:20:59 +0400

    the project is complete, full variable naming and scss have been added

---
+++ src/javascript/codeapp.js
@@ -0,0 +1,24 @@
+const timeFirst = document.querySelector('h1')
+let timeSecond = 15
+
+displayTime(timeSecond)
+
+const countDown = setInterval(()=>{
+	timeSecond--
+	displayTime(timeSecond)
+	if(timeSecond <= 0 || timeSecond <1 ){
+		timeIsOver()
+		clearInterval(countDown)
+	}
+},1000)
+
+function displayTime(second){
+	const minutes = Math.floor(second /60) 
+	const seconds = Math.floor(second % 60)
+
+	timeFirst.innerHTML = `${minutes <10 ? '0': ''}${minutes}:${seconds <10 ? '0': ''}${second}`
+}
+
+function timeIsOver(){
+	timeFirst.innerHTML = 'TIME OUT'
+}
